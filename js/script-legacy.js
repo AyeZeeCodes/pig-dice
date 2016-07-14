c359018ced2bae2d612483f618dbe197bcc6d661
Alex Zerbach and Dan Schafer <danfschafer@gmail.com>
2016-07-13 20:09:34 -0700

    add style, update button actions

---
+++ js/script-legacy.js
@@ -0,0 +1,54 @@
+//Randomly gives us a number from 1-6
+function rollDice() {
+  return Math.floor((Math.random() * 6) + 1);
+};
+
+//Creating the player constructor
+function Player(name, totalScore) {
+  this.name = name;
+  this.totalScore = 0;
+};
+//Defining player objects
+
+//var player1 = new Player ('Dan');
+//var player2 = new Player ('Alex');
+$('#playerSubmit').submit(function(event) {
+  event.prefentDefault;
+  var player1 = new Player($("#player1Name").val());
+  var player2 = new Player($("#player2Name").val());
+ });
+//Creates an empty array to store the turn's values
+var rollResult = [];
+
+// jQuery front end logic.
+$(function() {
+  $("#roll").click(function() {
+     var roll = parseInt(rollDice());
+     if(roll === 1) {
+       $("button#hold, button#roll").attr("disabled", "disabled");
+        //Improve Alert Message Styling
+         swal("Uh Oh! You Rolled A 1", "Next Player's Turn");
+         $('#nextPlayer').show();
+
+        } else {
+       rollResult.push(roll);
+       $('#lastRoll').show();
+       $('#rollCount').text(roll);
+     }
+  }); //Ends roll dice function
+  $("#hold").click(function() {
+    var turnScore = 0;
+    // Add up the roll values to get turnScore
+      rollResult.forEach(function(roll) {
+        turnScore += roll;
+          $("#result").show();
+          $("#turnScore").text(turnScore);
+          $("#p1Score").text(turnScore);
+          $("button#hold, button#roll").attr("disabled", "disabled");
+          $('#nextPlayer').show();
+
+          player1.totalScore = turnScore;
+      }); //Loops through each value in the array
+    }); //Ends hold button function
+
+}); //Ends jQuery document ready wrapper
--- js/script.js
+++ js/script.js
@@ -4,53 +4,76 @@ function rollDice() {
 };
 
 //Creating the player constructor
-function Player(name, totalScore) {
+function Player(name, turnScore, totalScore) {
   this.name = name;
+  this.turnScore = [];
   this.totalScore = 0;
 };
 //Defining player objects
 
 var player1 = new Player ('Dan');
 var player2 = new Player ('Alex');
-//$('#playerSubmit').submit(function(event) {
-//  event.prefentDefault;
-//  var player1 = new Player($("#player1Name").val());
-//  var player2 = new Player($("#player2Name").val());
+// $('form#playerSubmit').submit(function(event) {
+//   var player1 = new Player ($("input#player1Name").val())
+//   var player2 = new Player ($("input#player2Name").val())
+//   event.prefentDefault();
 //  });
 //Creates an empty array to store the turn's values
-var rollResult = [];
+//var rollResult = [];
 
-// jQuery
+//Switches player's turn
+
+var player_index = 1;
+var player = 'player1';
+console.log(player);
+function getPlayer() {
+  if (player_index%2 !== 0) {
+    player = player1;
+  } else {
+      player = player2;
+    };
+    return player;
+  };
+  function outputPlayerScore() {
+      if (player === player1) {
+        $("#player1").append(score);
+      } else {
+        $("#player2").append(score);
+      };
+  };
+// jQuery front end logic.
 $(function() {
+
   $("#roll").click(function() {
+    getPlayer();
+    console.log(player);
      var roll = parseInt(rollDice());
      if(roll === 1) {
-       $("button#hold, button#roll").attr("disabled", "disabled");
+       //$("button#hold, button#roll").attr("disabled", "disabled");
         //Improve Alert Message Styling
          swal("Uh Oh! You Rolled A 1", "Next Player's Turn");
-         $('#nextPlayer').show();
-
+         return;
         } else {
-       rollResult.push(roll);
+       player.turnScore.push(roll);
        $('#lastRoll').show();
        $('#rollCount').text(roll);
      }
   }); //Ends roll dice function
-  $("#hold").click(function() {
-    var turnScore = 0;
-    // Add up the roll values to get turnScore
-      rollResult.forEach(function(roll) {
-        turnScore += roll;
-          $("#result").show();
-          $("#turnScore").text(turnScore);
-          $("#p1Score").text(turnScore);
-          $("button#hold, button#roll").attr("disabled", "disabled");
-          $('#nextPlayer').show();
 
-          player1.totalScore = turnScore;
+
+  $("#hold").click(function() {
+    getPlayer();
+    var score = 0;
+    // Add up the roll values to get turnScore
+      player.turnScore.forEach(function(roll) {
+        score += roll;
+        $("#turnResult").show();
+        $("#score").text(score);
+            //$("button#hold, button#roll").attr("disabled", "disabled");
+          player.totalScore = score;
       }); //Loops through each value in the array
+      outputPlayerScore();
+      player_index += 1;
+      console.log(player_index);
     }); //Ends hold button function
-    $('#nextPlayer').click(function() {
-    location.reload();
-});
 }); //Ends jQuery document ready wrapper
